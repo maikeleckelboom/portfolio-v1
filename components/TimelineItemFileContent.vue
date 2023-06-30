4e4b4622de2f5e75be08633ef5683671d4ee9eee
Maikel Eckelboom <57023072+maikeleckelboom@users.noreply.github.com>
2023-06-30 03:29:40 +0200

    feat: menu at timeline/slug and file things

---
+++ components/TimelineItemFileContent.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import {ITimelineItem} from "~/types/portfolio";
+
+const props = defineProps<{
+  data: ITimelineItem
+}>()
+</script>
+
+<template>
+  <h1 class="mb-4 text-headline-medium">
+    <slot name="title">Schermafbeeldingen</slot>
+  </h1>
+  <div class="grid grid-cols-3 gap-4"
+       v-if="data?.timeline_files?.length">
+    <NuxtLink
+        v-for="attachment in data.timeline_files"
+        :to="`/timeline/${data.slug}/files/${attachment.file.id}`"
+        class="relative flex h-full max-h-[160px] min-h-[80px] w-full min-w-full overflow-hidden rounded-lg border outline-offset-2"
+    >
+      <NuxtImg
+          :alt="attachment.file.filename"
+          :src="attachment.file.filepath"
+          class="mx-auto rounded-md object-cover group-hover:opacity-80"
+      />
+    </NuxtLink>
+  </div>
+</template>
---
+++ components/TimelineItemTextContent.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import {ITimelineItem} from "~/types/portfolio";
+
+const props = defineProps<{
+  data: ITimelineItem
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col px-2">
+
+    <fieldset class="grid grid-cols-2 gap-4">
+      <div class="flex flex-col">
+        <sub
+            class="mb-1.5 flex -skew-x-6 text-label-small text-on-surface-variant md:text-label-medium"
+        >
+          Functietitel
+        </sub>
+        <h1 class="mb-4 text-body-large">{{ data.roleName }}</h1>
+      </div>
+      <div class="flex flex-col">
+        <sub
+            class="mb-1.5 flex -skew-x-6 text-label-small text-on-surface-variant md:text-label-medium"
+        >
+          Dienstverband
+        </sub>
+        <h1 class="mb-4 text-body-large capitalize">
+          {{ data.type === 'job' ? 'Baan' : 'Stage' }}
+          <span>- {{ data.contractType }}</span>
+        </h1>
+      </div>
+    </fieldset>
+    <fieldset class="grid grid-cols-2 gap-4">
+      <div class="flex flex-col">
+        <sub
+            class="mb-1.5 flex -skew-x-6 text-label-small text-on-surface-variant md:text-label-medium"
+        >
+          Bedrijf
+        </sub>
+        <h1 class="mb-4 text-body-large">{{ data.companyName }}</h1>
+      </div>
+      <div class="flex flex-col">
+        <sub
+            class="mb-1.5 flex -skew-x-6 text-label-small text-on-surface-variant md:text-label-medium"
+        >
+          Locatie
+        </sub>
+        <h1 class="mb-4 text-body-large">{{ data.companyLocation }}</h1>
+      </div>
+    </fieldset>
+    <fieldset>
+      <sub
+          class="mb-1.5 flex -skew-x-6 text-label-small text-on-surface-variant md:text-label-medium"
+      >
+        Bedrijfsomschrijving
+      </sub>
+      <h1 class="mb-4 text-body-large">
+        {{ data.companyDescription }}
+      </h1>
+    </fieldset>
+    <fieldset>
+      <sub
+          class="mb-1.5 flex -skew-x-6 text-label-small text-on-surface-variant md:text-label-medium"
+      >
+        Periode
+      </sub>
+      <h1 class="mb-4 text-body-large">
+        {{ data.dates.start }} - {{ data.dates.end }} ({{
+        data.dates.duration
+        }})
+      </h1>
+    </fieldset>
+    <fieldset>
+      <sub
+          class="mb-1.5 flex flex-col -skew-x-6 text-label-small text-on-surface-variant md:text-label-medium"
+      >
+        Taken en verantwoordelijkheden
+      </sub>
+      <ul class="">
+        <li
+            v-for="listItem in data.roleDescription.split('--').slice(1)"
+            class="list-disc pl-5 text-body-large"
+        >
+          {{ listItem }}
+        </li>
+      </ul>
+    </fieldset>
+    <fieldset class="mt-6 flex">
+      <TheTags :tags="data.tags"/>
+    </fieldset>
+  </div>
+</template>
